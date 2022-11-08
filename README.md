afa1e5a1a100b16f491f12965ecfdbf3e882f9e0
Freewind <nowindlee@gmail.com>
2022-11-08 19:01:47 +0800

    typescript-vite-valtio-check-if-object-change-if-field-change-demo

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
-TypeScript Vite React Valtio State Management Demo
+TypeScript Vite React Valtio If Object Change if Field Changes Demo
 ===========================
 
-由于真正工作的是proxy，所以如果引用到了非proxy字段，就不会起作用。
+如果store中某个字段变了，它的parent object都会变，而无关的object不会变。
 
-几个注意点：
+这对于hook如useMemo/useEffect来说是很友好的
 
-1. 对象中可以使用`as type`来明确类型
-2. action中修改字段时，要引用proxy之后的对象
-3. 使用store时，必须使用`store.action`风格，而不能先解构为`const {action} = store; action()`。因为这种情况下，proxy也会失效。
-
-最好按这里提示的方法来组织 actions: 
-https://github.com/pmndrs/valtio/wiki/How-to-organize-actions
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
 It will open page on browser automatically.
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,23 +1,36 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import './Hello.pcss';
 import {proxy, useSnapshot} from 'valtio';
 
 const store = proxy({
-    user: 'AAA' as string,
+    user: {
+      name: 'aaa'
+    },
+    user2: {
+      name: 'bbb'
+    },
     changeName: (value: string) => {
       console.log("### new value: ", value)
       // Use `store` here
-      store.user = value
+      store.user.name = value
     },
     hello: () => console.log(`Hello, ${store.user}`)
   }
 );
 
 export const Hello: FC = () => {
-  const {user} = useSnapshot(store);
+  const snap = useSnapshot(store);
+
+  useEffect(() => {
+    console.log("### snap.user is changed", snap.user);
+  }, [snap.user])
+
+  useEffect(() => {
+    console.log("### snap.user2 is changed", snap.user2);
+  }, [snap.user2])
 
   return <div className={'Hello'}>
-    <h1>Hello {user}</h1>
-    <input type={'text'} value={user} onChange={(event) => store.changeName(event.target.value)}/>
+    <h1>Hello {snap.user.name}</h1>
+    <input type={'text'} value={snap.user.name} onChange={(event) => store.changeName(event.target.value)}/>
   </div>;
 }
